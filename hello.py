9869588ffb3a2d15da163dd24082f004114e51e7
Ilia <kul.2002.il@gmail.com>
2020-12-20 12:06:49 +0700

    Обработка шаблонов и методов запросов.

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return 'hello'
+	if request.method == 'POST':
+		return 'hello, post'
+	return render_template('index.html')
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
